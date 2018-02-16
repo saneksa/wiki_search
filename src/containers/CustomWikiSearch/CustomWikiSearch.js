8034d9b595e99be1b2e86cb6fd5802dd6d08e7a8
Alex <saneksa2@gmail.com>
2018-02-16 15:42:22 +0300

    Реализация Smart и Dumb компонентов

--- src/containers/CustomWikiSearch/CustomWikiSearch.js
+++ src/containers/CustomWikiSearch/CustomWikiSearch.js
@@ -1,13 +1,10 @@
 import * as React from 'react';
 import * as PropTypes from 'react/lib/ReactPropTypes';
 import {connect} from 'react-redux';
-import * as _ from 'lodash';
-
 import {ConnectedSearchForm} from '../../components/SearchForm/SearchForm';
-import {searchWiki,linkUpdate} from '../../actions/wikiSearch';
-import {ConnectedLinksUncheck} from '../../components/Links/LinksUncheck';
-import {ConnectedLinksCheck} from '../../components/Links/LinksCheck';
-
+import {searchWiki, linkUpdate, linkCheckToLogic, linkUncheckToLogic} from '../../actions/wikiSearch';
+import LinksUncheck from '../../components/Links/LinksUncheck'
+import LinksCheck from "../../components/Links/LinksCheck";
 
 export class CustomWikiSearch extends React.Component {
     static propTypes = {
@@ -19,20 +16,18 @@ export class CustomWikiSearch extends React.Component {
 
     constructor(...args) {
         super(...args);
-
         this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
-
     }
 
     render() {
-
         return (
             <div style={{display: 'block'}}>
                 <ConnectedSearchForm form="wikiSearch" onSubmit={this.handleSearchSubmit}/>
-                <ConnectedLinksUncheck style={{display: 'block', marginRight: '700px'}}/>
-                <ConnectedLinksCheck style={{display: 'block'}}/>
+                <LinksUncheck links_c = {this.props.links_c} data = {this.props.linkUncheck}
+                                       style={{display: 'block', marginRight: '700px'}}/>
+                <LinksCheck links_u={this.props.links_u} data = {this.props.linkCheck}
+                                     style={{display: 'block'}}/>
             </div>
-
         );
     }
 
@@ -41,12 +36,12 @@ export class CustomWikiSearch extends React.Component {
             this.props.fetchData(values.search);
         }
     }
-
 }
 
 const mapStateToProps = state => ({
     searchResults: state.wikiSearch && state.wikiSearch.data,
-    links: state.links
+    linkUncheck: state.links.linksUncheck,
+    linkCheck: state.links.linksCheck
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -55,15 +50,12 @@ const mapDispatchToProps = dispatch => ({
     },
 
     links_c(check_links) {
-        dispatch(linkCheck(check_links));
-
+        dispatch(linkCheckToLogic(check_links));
     },
 
     links_u(uncheck_links) {
-        dispatch(linkUncheck(uncheck_links));
-
+        dispatch(linkUncheckToLogic(uncheck_links));
     }
-
 });
 
 export const ConnectedWikiSearch = connect(mapStateToProps, mapDispatchToProps)(CustomWikiSearch);
